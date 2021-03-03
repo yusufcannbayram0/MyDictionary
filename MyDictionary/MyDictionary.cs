c631b4e4234d80b9215247b09824197513993d02
YB <YB@DESKTOP-L3B9R2M>
2021-03-03 13:59:26 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<KT ,VT >
+    {
+        KT[] _keys;
+        VT[] _values;
+
+        public MyDictionary()
+        {
+            _keys = new KT[0];
+            _values = new VT[0];
+        }
+
+        public void Add(KT key,VT value)
+        {
+            KT[] tempKeys = _keys;
+            _keys = new KT[_keys.Length + 1];
+
+            VT[] tempValues = _values;
+            _values = new VT[_values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                _keys[i] = tempKeys[i];
+            }
+            _keys[_keys.Length - 1] = key;
+
+
+            for (int j = 0; j < tempValues.Length; j++)
+            {
+                _values[j] = tempValues[j];
+            }
+            _values[_values.Length - 1] = value;
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,string> ogrenci = new MyDictionary<string,string>();
+            ogrenci.Add("Masa","Table");
+            ogrenci.Add("Kalem", "Pen");
+            ogrenci.Add("Ayakkabı", "Shoe");
+            ogrenci.Add("Bilgisayar", "Computer");
+
+        }
+    }
+}
